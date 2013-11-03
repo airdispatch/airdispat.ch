ae3db9e8760f1365d83136cc22b4698b353f3a49
Hunter Leath <h@hunterleath.com>
2013-11-02 22:44:11 -0400

    Refinished AD Homepage

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 [![Build Status](https://drone.io/github.com/airdispatch/project-site/status.png)](https://drone.io/github.com/airdispatch/project-site/latest)
 
 This project controls the [Air Dispatch Homepage](http://airdispat.ch). It is very simple - only controlling an index page and an AD Blog.
+
+`go run webserver.go -port :xxxx`
--- webserver.go
+++ webserver.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"flag"
-	"os"
-	"github.com/airdispatch/blog"
 	"github.com/airdispatch/go-pressure"
-	"airdispat.ch/common"
+	"os"
+	"path/filepath"
 )
 
-var WORKING_DIRECTORY string
-var TEMPLATE_DIRECTORY string
+// var WORKING_DIRECTORY string
+// var TEMPLATE_DIRECTORY string
 var PORT string
 
 var flag_port = flag.String("port", "", "specify the port that the server should run on")
 var blog_address = flag.String("blog_address", "", "specify the address to load the blog from")
+var debug = flag.Bool("debug", false, "specify whether you want to debug the program or not")
 
 func main() {
 	flag.Parse()
@@ -23,44 +23,66 @@ func main() {
 		temp_port = *flag_port
 	}
 
-	temp_blog := os.Getenv("BLOG_ADDRESS")
-	if temp_blog == "" {
-		temp_blog = *blog_address
-	}
+	temp_wd, _ := os.Getwd()
 
-	theServer := &pressure.Server {
-		Port: temp_port,
-	}
-	theServer.ConfigServer()
-	
-	webInit(temp_blog)
+	// Get Relevant Paths
+	template_dir := filepath.Join(temp_wd, "templates")
+	static_dir := filepath.Join(temp_wd, "static")
 
-	defineRoutes(theServer)
+	// Create Server and Necessary Engines
+	theServer := pressure.CreateServer(temp_port, *debug)
+	tEng := theServer.CreateTemplateEngine(template_dir, "base.html")
 
+	// Register Golang Import URLs
+	theServer.RegisterURL(
+		pressure.NewURLRoute("^/airdispatch$", &GolangFetchController{"airdispatch-protocol"}),
+		pressure.NewURLRoute("^/common$", &GolangFetchController{"airdispatch-protocol"}),
+		pressure.NewURLRoute("^/server$", &GolangFetchController{"airdispatch-protocol"}),
+		pressure.NewURLRoute("^/client$", &GolangFetchController{"airdispatch-protocol"}),
+		pressure.NewURLRoute("^/tracker$", &GolangFetchController{"airdispatch-protocol"}),
+		pressure.NewURLRoute("^/server/framework$", &GolangFetchController{"airdispatch-protocol"}),
+		pressure.NewURLRoute("^/client/framework$", &GolangFetchController{"airdispatch-protocol"}),
+		pressure.NewURLRoute("^/tracker/framework$", &GolangFetchController{"airdispatch-protocol"}),
+	)
+
+	// Register URLs
+	theServer.RegisterURL(
+		pressure.NewURLRoute("^/project/airdispatch", &ProjectController{tEng}),
+		pressure.NewURLRoute("^/$", &HomepageController{tEng}),
+		pressure.NewStaticFileRoute("^/static/", static_dir),
+	)
+
+	// Start the Server
 	theServer.RunServer()
 }
 
-var theBlog *blog.Blog
+// Define Custom Controllers
 
-func webInit(blogAddr string) {
-	serverKey, _ := common.CreateADKey()
-
-	theBlog =  &blog.Blog{
-		Address: blogAddr,
-		Trackers: []string{"mailserver.airdispat.ch:1024", "localhost:1024"},
-		Key: serverKey,
-		BlogId: "ad",
-	}
-	theBlog.Initialize()
+type ProjectController struct {
+	tEng *pressure.TemplateEngine
 }
 
-func defineRoutes(s *pressure.Server) {
-	s.WebServer.Get("/", s.DisplayTemplate("index.html"))
+func (c *ProjectController) GetResponse(p *pressure.Request, l *pressure.Logger) (pressure.View, *pressure.HTTPError) {
+	return c.tEng.NewTemplateView("projects/project.html", nil), nil
+}
 
-	blogTemp, err := s.GetTemplateNamed("blog.html")
-	if err != nil {
-		return
-	}
+type GolangFetchController struct {
+	packageName string
+}
 
-	s.WebServer.Get("/blog(.*)", theBlog.WebGoBlog(blogTemp, "base"))
+func (c *GolangFetchController) GetResponse(p *pressure.Request, l *pressure.Logger) (pressure.View, *pressure.HTTPError) {
+	return pressure.NewHTMLView(
+		`<html>
+			<head>
+				<meta name="go-import" content="airdispat.ch git https://github.com/airdispatch/` + c.packageName + `">
+			</head>
+		</html>`), nil
+}
+
+type HomepageController struct {
+	tEng *pressure.TemplateEngine
+}
+
+func (c *HomepageController) GetResponse(p *pressure.Request, l *pressure.Logger) (pressure.View, *pressure.HTTPError) {
+	return c.tEng.NewTemplateView("index.html", nil), nil
 }
